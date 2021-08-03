2807292765edfd94ab8d925a0bfee47e765d51ee
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-08-03 11:22:43 -0400

    add app inicio

---
+++ inicio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('', LandingView.as_view()),
+    path('enconstruccion/', EsperaView.as_view()),
+    path('inicio/', HomeView.as_view(), name='inicio'),
+    path('quees/', QueesView.as_view(), name='quees'),
+]
--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+
+class LandingView(TemplateView):
+    template_name = 'inicio/presentacion.html'
+
+class EsperaView(TemplateView):
+    template_name = 'inicio/en_construccion.html'
+
+class HomeView(TemplateView):
+    template_name = 'inicio/home.html'
+
+class QueesView(TemplateView):
+    template_name = 'inicio/que_es.html'
