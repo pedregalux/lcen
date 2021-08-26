6090dcbcba750749492741b98c21eb5b5a19d5d8
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-08-26 16:15:03 -0400

    modifica admin de custom users

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from .models import User, Ciudadano, Organizacion, Convencional
 
-admin.site.register(User)
+
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('nombre','is_ciudadano','is_organizacion','is_convencional',)}),
+    )
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Ciudadano)
 admin.site.register(Organizacion)
 admin.site.register(Convencional)
