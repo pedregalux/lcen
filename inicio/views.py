5ec59f997037ca9cb0764177293ab1e43bbf2f29
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-08-30 20:52:06 -0400

    logos orgs, fix home

--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from propuestas.models import Propuesta
 from django.views.generic import TemplateView
 
 
@@ -7,14 +8,27 @@ from django.views.generic import TemplateView
 class LandingView(TemplateView):
     template_name = 'inicio/presentacion.html'
 
+
+
 class EsperaView(TemplateView):
     template_name = 'inicio/en_construccion.html'
+
+
 
 class HomeView(TemplateView):
     template_name = 'inicio/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['propuestas'] = Propuesta.objects.order_by('-created')[:3]
+        return context
+
+
+
 class QueesView(TemplateView):
     template_name = 'inicio/que_es.html'
+
+
 
 class PreguntasView(TemplateView):
     template_name = 'inicio/preguntas-frecuentes.html'
