e5d4a784580dd4932b5bd5abc21e5e2512acc7dc
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-09-06 10:01:33 -0300

    reset pass 2

--- usuarios/views.py
+++ usuarios/views.py
@@ -224,12 +224,12 @@ def password_reset_request(request):
 					email_template_name = "password_reset_email.txt"
 					c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'dev.laconstitucionesnuestra.cl',
 					'site_name': 'LCeN',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
 					'token': default_token_generator.make_token(user),
-					'protocol': 'http',
+					'protocol': 'https',
 					}
 					email = render_to_string(email_template_name, c)
 					try:
