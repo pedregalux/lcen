fc745c1e524081c7513e8fb738b86d3700ac6410
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-09-09 22:35:22 -0300

    fix filtro propuesta

--- propuestas/filters.py
+++ propuestas/filters.py
@@ -5,8 +5,8 @@ from mantenedores.models import *
 
 
 class PropuestaTemas(django_filters.FilterSet):
-    tema = filters.ModelChoiceFilter(queryset=TemaPropuesta.objects.all().distinct(), label='', empty_label='Ordenar por Tema...')
-    comuna = filters.ModelChoiceFilter(queryset=Comuna.objects.all().distinct(), label='', empty_label='Ordenar por Comuna...')
+    tema = filters.ModelChoiceFilter(queryset=TemaPropuesta.objects.all().distinct(), label='', empty_label='Buscar por Tema...')
+    comuna = filters.ModelChoiceFilter(queryset=Comuna.objects.all().distinct(), label='', empty_label='Buscar por Comuna...')
 
     class Meta:
         model = Propuesta
