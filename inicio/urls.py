99d75287faac3575ff7525b32932077633ed39d8
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-09-15 19:00:00 -0300

    error pages

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import *
+from django.conf.urls import handler404, handler500, handler403, handler400
 
 urlpatterns = [
     path('', HomeView.as_view(), name='inicio'),
