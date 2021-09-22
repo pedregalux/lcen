046a78378f036208243c22af1fbf22928c479d82
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-09-22 15:13:41 -0300

    prop export 2

--- propuestas/admin.py
+++ propuestas/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from import_export import resources
+from import_export import resources, fields
+from import_export.widgets import ManyToManyWidget
 from import_export.admin import ImportExportModelAdmin
 from .models import TemaPropuesta, SubtemaPropuesta, ComponenteConstitucion, Propuesta, ApoyoPropuesta, CompromisoPropuesta
 from usuarios.models import User
+from mantenedores.models import *
 
 
 
@@ -21,9 +23,15 @@ class SubtemaResource(resources.ModelResource):
 
 
 class PropuestaResource(resources.ModelResource):
-    class Meta:
-        model = Propuesta
-        fields = ('created','modified','autor','pais','region','comuna','tema','otros_temas','tema_extra','problema','situacion','componente','otras_organizaciones','organizaciones_de_propuesta','compromiso_convencionales','anexo_propuesta','anexo2_propuesta','anexo3_propuesta','link_extra1','link_extra2','titulo','apoyos','compromisos',)
+        titulo = fields.Field(attribute='titulo', column_name='Título')
+        titulo = fields.Field(attribute='tema', column_name='Tema Principal')
+        otros_temas = fields.Field(attribute='otros_temas', widget=ManyToManyWidget(SubtemaPropuesta, field='subtema_propuesta'), column_name='Subtemas')
+        titulo = fields.Field(attribute='tema_extra', column_name='Título')
+
+
+        class Meta:
+            model = Propuesta
+            fields = ('created','modified','autor','pais','region','comuna','tema_extra','problema','situacion','componente','otras_organizaciones','organizaciones_de_propuesta','compromiso_convencionales','anexo_propuesta','anexo2_propuesta','anexo3_propuesta','link_extra1','link_extra2','titulo','apoyos','compromisos',)
 
 
 class ApoyoPropuestaAdmin(admin.ModelAdmin):
@@ -43,6 +51,7 @@ class SubtemaAdmin(ImportExportModelAdmin):
 
 
 class PropuestaExport(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('titulo', 'autor')
     resource_class = PropuestaResource
 
 
