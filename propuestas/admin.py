83b64d411713bfca07cb7c7f8578ab2290ba6aaf
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-09-22 10:08:50 -0300

    export propuestas 1

--- propuestas/admin.py
+++ propuestas/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import TemaPropuesta, SubtemaPropuesta, ComponenteConstitucion, Propuesta, ApoyoPropuesta, CompromisoPropuesta
+from usuarios.models import User
 
 
 
@@ -17,6 +18,12 @@ class SubtemaResource(resources.ModelResource):
         model = SubtemaPropuesta
         import_id_fields = ('id',)
         fields = ('id','subtema_propuesta',)
+
+
+class PropuestaResource(resources.ModelResource):
+    class Meta:
+        model = Propuesta
+        fields = ('created','modified','autor','pais','region','comuna','tema','otros_temas','tema_extra','problema','situacion','componente','otras_organizaciones','organizaciones_de_propuesta','compromiso_convencionales','anexo_propuesta','anexo2_propuesta','anexo3_propuesta','link_extra1','link_extra2','titulo','apoyos','compromisos',)
 
 
 class ApoyoPropuestaAdmin(admin.ModelAdmin):
@@ -35,9 +42,13 @@ class SubtemaAdmin(ImportExportModelAdmin):
     resource_class = SubtemaResource
 
 
+class PropuestaExport(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = PropuestaResource
+
+
 admin.site.register(TemaPropuesta, TemaAdmin)
 admin.site.register(SubtemaPropuesta, SubtemaAdmin)
 admin.site.register(ComponenteConstitucion)
-admin.site.register(Propuesta)
+admin.site.register(Propuesta, PropuestaExport)
 admin.site.register(ApoyoPropuesta, ApoyoPropuestaAdmin)
 admin.site.register(CompromisoPropuesta, CompromisoPropuestaAdmin)
