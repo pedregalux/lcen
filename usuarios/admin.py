0d4c8d7c8f11d9508f2776626ce855b33227e27a
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-09-23 17:17:54 -0300

    export users 1

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
+from import_export import resources, fields
+from import_export.widgets import ManyToManyWidget, ForeignKeyWidget
+from import_export.admin import ImportExportModelAdmin
 from .models import User, Ciudadano, Organizacion, Convencional
+
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        exclude = ('password',)
+
 
 
 class UserChangeForm(UserChangeForm):
@@ -9,13 +19,12 @@ class UserChangeForm(UserChangeForm):
         model = User
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(UserAdmin, ImportExportModelAdmin):
+    resource_class = UserResource
     form = UserChangeForm
-
     fieldsets = UserAdmin.fieldsets + (
             (None, {'fields': ('nombre','is_ciudadano','is_organizacion','is_convencional',)}),
     )
-
 
 
 admin.site.register(User, UserAdmin)
