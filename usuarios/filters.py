5e3117b70b255b0cca363431897eef7dfe1b91f0
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-11-14 00:52:06 -0300

    apellidos

--- usuarios/filters.py
+++ usuarios/filters.py
@@ -8,10 +8,10 @@ from convencionales.models import *
 
 class ConvencionalesFiltro(django_filters.FilterSet):
 
-    lista = filters.ModelChoiceFilter(queryset=Lista.objects.all().distinct(), label='', empty_label='Buscar por Colectivo...')
-    distrito = filters.ModelChoiceFilter(queryset=Distrito.objects.all().distinct(), label='', empty_label='Buscar por Distrito...')
-    cargo = filters.ModelChoiceFilter(queryset=Cargo.objects.all().distinct(), label='', empty_label='Buscar por Comisión...')
-    email = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Buscar por Apellido'}), label='Buscar por Apellido')
+    lista = filters.ModelChoiceFilter(queryset=Lista.objects.all().distinct(), label='', empty_label='Buscar Colectivo')
+    distrito = filters.ModelChoiceFilter(queryset=Distrito.objects.all().distinct(), label='', empty_label='Buscar Distrito')
+    cargo = filters.ModelChoiceFilter(queryset=Cargo.objects.all().distinct(), label='', empty_label='Buscar Comisión')
+    email = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Buscar por Apellido'}), label='Buscar Apellido')
     # movimiento = filters.ModelChoiceFilter(queryset=Movimiento.objects.all().distinct(), label='', empty_label='Buscar por Partido/Movimiento...')
 
 
