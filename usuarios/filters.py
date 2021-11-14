ded1baef1d182f78627831c0cc27775519acfe1e
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2021-11-14 00:43:35 -0300

    filtro apellidos

--- usuarios/filters.py
+++ usuarios/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from django_filters import filters
+from django.forms.widgets import TextInput
 from .models import User, Convencional
 from mantenedores.models import *
 from convencionales.models import *
@@ -10,7 +11,7 @@ class ConvencionalesFiltro(django_filters.FilterSet):
     lista = filters.ModelChoiceFilter(queryset=Lista.objects.all().distinct(), label='', empty_label='Buscar por Colectivo...')
     distrito = filters.ModelChoiceFilter(queryset=Distrito.objects.all().distinct(), label='', empty_label='Buscar por Distrito...')
     cargo = filters.ModelChoiceFilter(queryset=Cargo.objects.all().distinct(), label='', empty_label='Buscar por Comisión...')
-    email = django_filters.CharFilter(lookup_expr='icontains', label='Buscar por Apellido')
+    email = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Buscar por Apellido'}), label='Buscar por Apellido')
     # movimiento = filters.ModelChoiceFilter(queryset=Movimiento.objects.all().distinct(), label='', empty_label='Buscar por Partido/Movimiento...')
 
 
