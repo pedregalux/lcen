9972832147e8229f705138900bfd8086dc2a7246
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2022-05-24 12:31:58 -0400

    evaluaciones views

--- evaluaciones/views.py
+++ evaluaciones/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .models import Norma
 
-# Create your views here.
+
+class VerEvaluacionesView(ListView):
+    model = Norma
+    context_object_name = 'evaluaciones_list'
+    template_name = 'evaluaciones/verevaluaciones.html'
+
+
+class VerEvaluacionView(DetailView):
+    model = Norma
+    context_object_name = 'evaluacion_detail'
+    template_name = 'evaluaciones/verevaluacion.html'
