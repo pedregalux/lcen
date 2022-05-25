d1256022af1b1f5440634fdec26f207268a9f0ea
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2022-05-24 20:39:11 -0400

    fix norma

--- evaluaciones/models.py
+++ evaluaciones/models.py
@@ -80,7 +80,7 @@ class Norma(models.Model):
         related_name="tags_de_norma",
         verbose_name="Tags/Sellos aplicados a la norma",
         blank=True)
-    titulo_oficial_norma = models.TextField("Título oficial de norma",
+    titulo_oficial_norma = models.CharField("Título oficial de norma",
         max_length=256,
         help_text="Título oficial de la norma")
     titulo_web_norma = models.CharField("Título web de la norma",
