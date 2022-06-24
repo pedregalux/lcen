5d8a3d291c3c5f67100ee3e3c98320c8a67b14ab
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2022-06-24 15:22:48 -0400

    admin evaluaciones

--- evaluaciones/admin.py
+++ evaluaciones/admin.py
@@ -1,8 +1,166 @@
 from django.contrib import admin
+from import_export import resources, fields
+from import_export.widgets import ManyToManyWidget, ForeignKeyWidget
+from import_export.admin import ImportExportModelAdmin
 from .models import OrganizacionNorma,CategoriaNorma,TagNorma,Norma
 
 
-admin.site.register(OrganizacionNorma)
-admin.site.register(CategoriaNorma)
-admin.site.register(TagNorma)
+class OrganizacionNormaResource(resources.ModelResource):
+    class Meta:
+        model = OrganizacionNorma
+        import_id_fields = ('id',)
+        fields = ('id','nombre_org_norma','logo_org_norma',)
+
+
+class CategoriaNormaResource(resources.ModelResource):
+    class Meta:
+        model = CategoriaNorma
+        import_id_fields = ('id',)
+        fields = ('id','tema_norma',)
+
+
+class TagNormaResource(resources.ModelResource):
+    class Meta:
+        model = TagNorma
+        import_id_fields = ('id',)
+        fields = ('id','tag_norma','icono_tag',)
+
+
+class NormaResource(resources.ModelResource):
+    org_evaluadora = fields.Field(attribute='org_evaluadora', widget=ForeignKeyWidget(OrganizacionNorma, field='org_evaluadora'), column_name='org_evaluadora')
+    categoriadelanorma = fields.Field(attribute='categoriadelanorma', widget=ForeignKeyWidget(CategoriaNorma, field='categoriadelanorma'), column_name='categoriadelanorma')
+    tags_norma = fields.Field(attribute='tags_norma', widget=ManyToManyWidget(TagNorma, field='tags_norma'), column_name='tags_norma')
+
+
+    class Meta:
+        model = Norma
+        import_id_fields = ('id',)
+        fields = ('id','nombre_org_norma','logo_org_norma',)
+
+
+# La norma
+class Norma(models.Model):
+    org_evaluadora = models.ForeignKey(OrganizacionNorma,
+        related_name="organizacion_eval_norma",
+        verbose_name="Organización evaluadora",
+        null=True,
+        on_delete=models.SET_NULL)
+    categoriadelanorma = models.ForeignKey(CategoriaNorma,
+        related_name="categoria_eval_norma",
+        verbose_name="Categoría de la norma",
+        null=True,
+        on_delete=models.SET_NULL)
+    tags_norma = models.ManyToManyField(TagNorma,
+        related_name="tags_de_norma",
+        verbose_name="Tags/Sellos aplicados a la norma",
+        blank=True)
+    titulo_oficial_norma = models.CharField("Título oficial de norma",
+        max_length=256,
+        help_text="Título oficial de la norma")
+    titulo_web_norma = models.CharField("Título web de la norma",
+        max_length=256,
+        help_text="Título oficial de la norma")
+    resumen_norma = models.TextField("Resumen de la norma",
+        help_text="Resumen de la norma")
+    url = models.URLField(
+        "URL de la Norma",
+        null=True,
+        blank=True,
+        help_text="URL de la Norma")
+    importancia_para_chile = models.ImageField("Importancia para Chile",
+        upload_to='iconostemas/',
+        null=True,
+        blank=True,
+        help_text="¿Cuán importante es este artículo(s) para la calidad de vida de las personas en Chile?")
+    razones_importancia_norma = models.TextField("Razones de importancia de norma",
+        null=True,
+        blank=True,
+        help_text="¿Por qué este artículo(s) tiene la importancia para la calidad de vida que indicaste en la pregunta anterior?")
+    mejora_de_cp80 = models.ImageField("Mejora de constitución anterior",
+        upload_to='iconostemas/',
+        null=True,
+        blank=True,
+        help_text="En el marco de una agenda de justicia social, económica y de derechos humanos: ¿en qué medida la Nueva Constitución mejora respecto de la Constitución actualmente vigente?")
+    razones_mejora_cp80 = models.TextField("Razones de mejora de constitución anterior",
+        null=True,
+        blank=True,
+        help_text="¿Cómo puedes justificar la opción asignada en la pregunta anterior?")
+    presencia_otros_paises = models.TextField("Presencia en otros países",
+        null=True,
+        blank=True,
+        help_text="¿Hay presencia de artículos similares en otros países? Explícanos en qué países y en qué se parecen")
+    historia_norma = models.TextField("Historia de la norma",
+        null=True,
+        blank=True,
+        help_text="¿Cuál es la historia de este artículo en términos de su creación social?")
+    mitos_norma = models.TextField("Mitos de la norma",
+        null=True,
+        blank=True,
+        help_text="¿Cuáles son los principales mitos asociados al artículo(s) y porqué son mitos?")
+    futurista_norma = models.TextField("Futuro de la norma",
+        null=True,
+        blank=True,
+        help_text="Imagina que estamos en 2052: ¿Cómo es tu entorno y la vida de las personas en Chile después de este artículo(s)?")
+    aporte_desarrollo = models.TextField("Aporte al desarrollo",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta al desarrollo del país?")
+    aporte_ddhh_natura = models.TextField("Aporte DDHH y naturaleza",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta al respeto a los derechos humanos y de la naturaleza?")
+    aporte_seguridad = models.TextField("Aporte seguridad y certeza",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta a la seguridad y certeza de las personas en Chile?")
+    aporte_igualdad = models.TextField("Aporte a la igualdad",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta a la igualdad entre todas las personas?")
+    aporte_innovacion = models.TextField("Aporte a la innovación y sustentabilidad",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta a la innovación y sustentabilidad?")
+    aporte_descentralizacion = models.TextField("Aporte a la descentralización",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta a los procesos de descentralización?")
+    aporte_contra_corrupcion = models.TextField("Aporte contra la corrupción",
+        null=True,
+        blank=True,
+        help_text="¿En qué forma el artículo(s) analizado aporta a los procesos de participación y a la lucha contra la corrupción?")
+    anexo_norma_pdf = models.FileField(upload_to='documents/',
+        verbose_name="Documento del artículo",
+        null=True,
+        blank=True)
+    anexo_norma_png = models.FileField(upload_to='documents/',
+        verbose_name="Ilustración del artículo",
+        null=True,
+        blank=True)
+    sello_norma = models.ImageField("Imagen/Sello de Norma", upload_to='iconostemas/', null=True, blank=True)
+    prev_norma = models.ForeignKey("self", related_name="norma_prev", null=True, blank=True, on_delete=models.CASCADE)
+    next_norma = models.ForeignKey("self", related_name="norma_next", null=True, blank=True, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+class OrganizacionNormaAdmin(ImportExportModelAdmin):
+    resource_class = OrganizacionNormaResource
+
+
+class CategoriaNormaAdmin(ImportExportModelAdmin):
+    resource_class = CategoriaNormaResource
+
+
+class TagNormaAdmin(ImportExportModelAdmin):
+    resource_class = TagNormaResource
+
+admin.site.register(OrganizacionNorma, OrganizacionNormaAdmin)
+admin.site.register(CategoriaNorma, CategoriaNormaAdmin)
+admin.site.register(TagNorma, TagNormaAdmin)
 admin.site.register(Norma)
