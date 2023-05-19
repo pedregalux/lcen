432cca28701604fe8d5d9e7ce74e9e893a768e2c
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2023-05-19 15:59:13 -0400

    fix forms para muestra

--- propuestas/forms.py
+++ propuestas/forms.py
@@ -16,9 +16,9 @@ class PropuestaForm1(forms.ModelForm):
         'comuna'
         ]
         labels = {
-            'pais': ('<h6><span style="text-transform:uppercase">1. País</span></h6><i>Selecciona el país desde donde ingresas la propuesta</i>'),
-            'region': ('<h6><span style="text-transform:uppercase">2. Región</span></h6><i>Selecciona la región relacionada con la propuesta, si corresponde</i>'),
-            'comuna': ('<h6><span style="text-transform:uppercase">3. Comuna</span></h6><i>Selecciona la comuna relacionada con la propuesta, si corresponde</i>'),
+            'pais': ('1. País -- Selecciona el país desde donde ingresas la propuesta'),
+            'region': ('2. Región -- Selecciona la región relacionada con la propuesta, si corresponde'),
+            'comuna': ('3. Comuna -- Selecciona la comuna relacionada con la propuesta, si corresponde'),
         }
 
 
@@ -32,9 +32,9 @@ class PropuestaForm2(forms.ModelForm):
         'tema_extra',
         ]
         labels = {
-            'tema': ('<h6><span style="text-transform:uppercase">1. ¿Cuál es el tema principal de tu propuesta?</span></h6><i>Selecciona el tema principal desde la lista desplegable</i>'),
-            'otros_temas': ('<h6><span style="text-transform:uppercase">2. ¿Qué otros temas aborda tu propuesta?</span></h6><i>Por favor selecciona hasta tres temas adicionales, debes seleccionarlos con el mouse y apretando la telca Control -Ctrl-</i>'),
-            'tema_extra': ('<h6><span style="text-transform:uppercase">3. Otro tema, ¿cuál?</span></h6><i>Si tu propuesta contempla algún tema que no está en el listado, por favor escríbelo aquí en una palabra o frase breve -máximo 100 cars.-</i>'),
+            'tema': ('1. ¿Cuál es el tema principal de tu propuesta? -- Selecciona el tema principal desde la lista desplegable'),
+            'otros_temas': ('2. ¿Qué otros temas aborda tu propuesta? -- Por favor selecciona hasta tres temas adicionales, debes seleccionarlos con el mouse y apretando la telca Control -Ctrl-'),
+            'tema_extra': ('3. Otro tema, ¿cuál? -- Si tu propuesta contempla algún tema que no está en el listado, por favor escríbelo aquí en una palabra o frase breve -máximo 100 cars.-'),
         }
 
 
@@ -48,9 +48,9 @@ class PropuestaForm3(forms.ModelForm):
         'componente'
         ]
         labels = {
-            'problema': ('<h6><span style="text-transform:uppercase">1. ¿Cuál es el problema que tú o tu organización busca solucionar?</span></h6><i>Ejemplo: El actual sistema de seguridad social no garantiza pensiones dignas para todas las personas.</i>'),
-            'situacion': ('<h6><span style="text-transform:uppercase">2. Con respecto al problema planteado, ¿cuál sería la situación ideal?</span></h6><i>Ejemplo: Cuando jubilan, todas las personas tienen acceso a una pensión que cubre sus necesidades básicas para vivir con dignidad.</i>'),
-            'componente': ('<h6><span style="text-transform:uppercase">3. Para avanzar en el logro de esa situación ideal, ¿qué debería contemplar la Nueva Constitución?</span></h6><i>Ejemplo: Consagrar constitucionalmente el derecho a la protección social y explicitar la obligación del Estado de asegurar a todas las personas una pensión digna.</i>'),
+            'problema': ('1. ¿Cuál es el problema que tú o tu organización busca solucionar? -- Ejemplo: El actual sistema de seguridad social no garantiza pensiones dignas para todas las personas.'),
+            'situacion': ('2. Con respecto al problema planteado, ¿cuál sería la situación ideal? -- Ejemplo: Cuando jubilan, todas las personas tienen acceso a una pensión que cubre sus necesidades básicas para vivir con dignidad.'),
+            'componente': ('3. Para avanzar en el logro de esa situación ideal, ¿qué debería contemplar la Nueva Constitución? -- Ejemplo: Consagrar constitucionalmente el derecho a la protección social y explicitar la obligación del Estado de asegurar a todas las personas una pensión digna.'),
         }
         widgets = {
             'problema': forms.Textarea(attrs={'placeholder': 'Describe el problema de manera precisa y breve en un máximo de 250 palabras.'}),
@@ -72,9 +72,9 @@ class PropuestaForm4(forms.ModelForm):
             'otras_organizaciones': forms.CheckboxInput()
         }
         labels = {
-            'otras_organizaciones': ('<h6><span style="text-transform:uppercase">1. ¿Esta propuesta fue elaborada en conjunto con otras organizaciones?</span></h6><i>Marque si es así.</i>'),
-            'organizaciones_de_propuesta': ('<h6><span style="text-transform:uppercase">2. Si tu respuesta fue “sí”, escribe cuáles:</span></h6><i>Por favor escribe los nombres de las organizaciones que participaron en la elaboración de la propuesta, separados por comas ¡No olvides incluir a tu organización!</i>'),
-            'organizaciones': ('<h6><span style="text-transform:uppercase">Sólo si eres una Organización: <br>¿Esta Propuesta involucra a otras organizaciones que están en La Constitución es Nuestra? Ten en cuenta que la cocreación de propuestas debe ser oficial, sino la propuesta será corregida</span></h6><i>Si la organización de la que formas parte está en La Constitución es Nuestra, debes ingresar la propuesta a través del perfil de la organización y no como un usuarix particular</i><br><i>Si eres una organización y quieres incluir en la creación de la propuesta a organizaciones adicionales, debes seleccionarlas con el mouse y apretando la telca Control -Ctrl-</i>'),
+            'otras_organizaciones': ('1. ¿Esta propuesta fue elaborada en conjunto con otras organizaciones? -- Marque si es así.'),
+            'organizaciones_de_propuesta': ('2. Si tu respuesta fue “sí”, escribe cuáles: -- Por favor escribe los nombres de las organizaciones que participaron en la elaboración de la propuesta, separados por comas ¡No olvides incluir a tu organización!'),
+            'organizaciones': ('Sólo si eres una Organización: ¿Esta Propuesta involucra a otras organizaciones que están en La Constitución es Nuestra? Ten en cuenta que la cocreación de propuestas debe ser oficial, sino la propuesta será corregida -- Si la organización de la que formas parte está en La Constitución es Nuestra, debes ingresar la propuesta a través del perfil de la organización y no como un usuarix particular. Si eres una organización y quieres incluir en la creación de la propuesta a organizaciones adicionales, debes seleccionarlas con el mouse y apretando la telca Control -Ctrl-'),
         }
 
 
@@ -90,8 +90,8 @@ class PropuestaForm4a(forms.ModelForm):
             'otras_organizaciones': forms.CheckboxInput()
         }
         labels = {
-            'otras_organizaciones': ('<h6><span style="text-transform:uppercase">1. ¿Esta propuesta fue elaborada en conjunto con otras organizaciones?</span></h6><i>Marque si es así.</i>'),
-            'organizaciones_de_propuesta': ('<h6><span style="text-transform:uppercase">2. Si tu respuesta fue “sí”, escribe cuáles:</span></h6><i>Por favor escribe los nombres de las organizaciones que participaron en la elaboración de la propuesta, separados por comas ¡No olvides incluir a tu organización!</i>'),
+            'otras_organizaciones': ('1. ¿Esta propuesta fue elaborada en conjunto con otras organizaciones? -- Marque si es así.'),
+            'organizaciones_de_propuesta': ('2. Si tu respuesta fue “sí”, escribe cuáles: -- Por favor escribe los nombres de las organizaciones que participaron en la elaboración de la propuesta, separados por comas ¡No olvides incluir a tu organización!'),
         }
 
 
@@ -107,8 +107,8 @@ class PropuestaForm5(forms.ModelForm):
             'compromiso_convencionales': forms.CheckboxInput()
         }
         labels = {
-            'compromiso_convencionales': ('<h6><span style="text-transform:uppercase">1. ¿Tu propuesta cuenta con compromisos formales de apoyo de convencionales constituyentes?</span></h6><i>Marque si es así.</i>'),
-            'anexo3_propuesta': ('<i>Si tienes un documento o archivo que acredite los compromisos, ingrésalo acá. Por favor no subir documentos con peso mayor a 10MB.</i>'),
+            'compromiso_convencionales': ('1. ¿Tu propuesta cuenta con compromisos formales de apoyo de convencionales constituyentes? -- Marque si es así.'),
+            'anexo3_propuesta': ('Si tienes un documento o archivo que acredite los compromisos, ingrésalo acá. Por favor no subir documentos con peso mayor a 10MB.'),
         }
 
 
@@ -123,10 +123,10 @@ class PropuestaForm6(forms.ModelForm):
         'link_extra2'
         ]
         labels = {
-            'anexo_propuesta': ('<h6><span style="text-transform:uppercase">Si has elaborado un documento con tu propuesta constitucional desarrollada en mayor detalle y profundidad, adjúntalo -no subir documentos con peso mayor a 10MB-:</span></h6>'),
-            'anexo2_propuesta': ('<i>Asimismo, si cuentas con otros materiales que complementen tu propuesta (estudios, encuestas, presentaciones, material comunicacional, etc.) , agrégalos acá. No subir documentos con peso mayor a 10MB.</i>'),
-            'link_extra1': ('<i>Si hay una página o documento online, ingresa un link a material complementario.</i>'),
-            'link_extra2': ('<i>Otro link a material complementario.</i>'),
+            'anexo_propuesta': ('Si has elaborado un documento con tu propuesta constitucional desarrollada en mayor detalle y profundidad, adjúntalo -no subir documentos con peso mayor a 10MB-:</span></h6>'),
+            'anexo2_propuesta': ('Asimismo, si cuentas con otros materiales que complementen tu propuesta (estudios, encuestas, presentaciones, material comunicacional, etc.) , agrégalos acá. No subir documentos con peso mayor a 10MB.'),
+            'link_extra1': ('Si hay una página o documento online, ingresa un link a material complementario.'),
+            'link_extra2': ('Otro link a material complementario.'),
         }
 
 
@@ -138,7 +138,7 @@ class PropuestaForm7(forms.ModelForm):
         'titulo'
         ]
         labels = {
-            'titulo': ('<h6><span style="text-transform:uppercase">Escribe un título atractivo para que podamos difundirla entre la ciudadanía y en la Convención Constitucional.</span></h6><i>Título: máximo 280 caracteres.</i>'),
+            'titulo': ('Escribe un título atractivo para que podamos difundirla entre la ciudadanía y en la Convención Constitucional. -- Título: máximo 280 caracteres.'),
         }
 
 
